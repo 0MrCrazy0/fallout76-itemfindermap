60c2c67c19fe92304fb7eeec8abfae6d9b6a2bfb
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2025-11-22 02:07:36 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,17 @@
 const CACHE = "fo76-finder-v76.1.0";
-
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll([
-      "/",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/index.html",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/manifest.json",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/icon-192-v2.png",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/icon-512-v2.png"
-      // add more files if you want, but your PWA already caches everything
+      "/fallout76-itemfindermap/",
+      "/fallout76-itemfindermap/index.html",
+      "/fallout76-itemfindermap/manifest.json",
+      "/fallout76-itemfindermap/icon-192-v2.png",
+      "/fallout76-itemfindermap/icon-512-v2.png"
     ]))
   );
 });
-
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
-
 });
-
-
-
