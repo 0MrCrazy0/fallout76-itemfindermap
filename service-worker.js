bf2159c205cca2b2f67c0da1c15483476c10bf1b
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2025-11-22 02:33:01 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ const CACHE = "fo76-finder-v76.1.0";
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll([
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/index.html",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/manifest.json",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/icon-192-v2.png",
-      "/0mrcrazy0.github.io/fallout76-itemfindermap/icon-512-v2.png"
+      "/fallout76-itemfindermap/",
+      "/fallout76-itemfindermap/index.html",
+      "/fallout76-itemfindermap/manifest.json",
+      "/fallout76-itemfindermap/icon-192-v2.png",
+      "/fallout76-itemfindermap/icon-512-v2.png"
     ]))
   );
 });
@@ -15,4 +15,5 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
+
 
