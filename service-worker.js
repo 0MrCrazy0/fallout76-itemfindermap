4a6c56e9979fbd38f5bc28ff17e0bcb7ad316b24
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2025-11-22 12:33:44 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener("install", e => {
     caches.open(CACHE).then(cache => cache.addAll([
       "/fallout76-itemfindermap/",
       "/fallout76-itemfindermap/index.html",
-      "/fallout76-itemfindermap/manifest042.json",
+      "/fallout76-itemfindermap/manifest.json",
       "/fallout76-itemfindermap/icon-192-v2.png",
       "/fallout76-itemfindermap/icon-512-v2.png",
       "/fallout76-itemfindermap/_headers",
@@ -18,6 +18,7 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
+
 
 
 
