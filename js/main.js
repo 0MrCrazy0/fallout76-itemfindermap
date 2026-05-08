d9f2bfca4a4943839d63a52a7d6703ca41f66504
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2026-05-09 01:16:30 +1000

    Update main.js
    76-Vault-OK-9-05-2026-Build-B-58

--- js/main.js
+++ js/main.js
@@ -1305,7 +1305,7 @@ window.exitFullscreenThenDo = function(callback) {
     const mapContainer = document.getElementById('map');
     if (!mapContainer) return;
 
-    const CACHE_NAME = "76-Vault-OK-9-05-2026-Build-B-57"; // must match service-worker.js
+    const CACHE_NAME = "76-Vault-OK-9-05-2026-Build-B-58"; // must match service-worker.js
     const MAP_IMAGES = [
     'https://cdn.jsdelivr.net/gh/0MrCrazy0/fallout76-itemfindermap@main/map-named.jpg?v=' + Date.now(),
     'https://cdn.jsdelivr.net/gh/0MrCrazy0/fallout76-itemfindermap@main/map-noname.jpg?v=' + Date.now()
